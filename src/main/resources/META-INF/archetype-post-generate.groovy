94efb8d81393d5b8d8d21cf9de47d0e9354e3b10
OsDev <satran004@gmail.com>
2019-03-23 00:16:41 +0800

    Auto fetch version number

---
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -0,0 +1,27 @@
+try {
+    if(request.isOffline()) {
+        println("Offline mode")
+        return
+    }
+
+    System.setProperty("goals", "io.takari:maven:wrapper")
+    def moduleDir = new File(request.getOutputDirectory()+"/"+request.getArtifactId())
+
+    def pomFile = new File(moduleDir, 'pom.xml')
+    println "Updating "+pomFile
+
+    def pomContent = pomFile.getText('UTF-8')
+
+    def replace = "x.x.x";
+
+    def version = new URL ("https://raw.githubusercontent.com/satran004/aion4j-maven-plugin/master/release_ver").getText([connectTimeout: 3000, readTimeout: 3000])
+
+    pomContent = pomContent.replace("x.x.x", version.trim())
+
+    // rewrite pom.xml
+    pomFile.newWriter().withWriter { w ->
+        w << pomContent
+    }
+} catch(Error e) {
+
+}
