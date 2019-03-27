aa193a90e70b75eb218ce554277222ca16ef52b1
OsDev <satran004@gmail.com>
2019-03-28 01:13:08 +0800

    ABIDecoder & ABIEncoder package change

--- src/main/resources/archetype-resources/src/main/java/HelloAvm.java
+++ src/main/resources/archetype-resources/src/main/java/HelloAvm.java
@@ -1,19 +1,19 @@
 package ${package};
 
-import org.aion.avm.api.ABIDecoder;
+import org.aion.avm.tooling.abi.Callable;
+import org.aion.avm.userlib.abi.ABIDecoder;
 import org.aion.avm.api.BlockchainRuntime;
 
 public class HelloAvm
 {
+    @Callable
     public static void sayHello() {
         BlockchainRuntime.println("Hello Avm");
     }
 
+    @Callable
     public static String greet(String name) {
         return "Hello " + name;
     }
 
-    public static byte[] main() {
-        return ABIDecoder.decodeAndRunWithClass(HelloAvm.class, BlockchainRuntime.getData());
-    }
 }
--- src/main/resources/archetype-resources/src/test/java/HelloAvmRuleTest.java
+++ src/main/resources/archetype-resources/src/test/java/HelloAvmRuleTest.java
@@ -1,6 +1,6 @@
 package ${package};
 
-import org.aion.avm.api.ABIEncoder;
+import org.aion.avm.userlib.abi.ABIEncoder;
 import org.aion.avm.api.Address;
 import org.aion.avm.tooling.AvmRule;
 import org.aion.vm.api.interfaces.ResultCode;
